451fd20306b0b51e327d487005946ef1cb8befa1
zachbryant <zbryant832@gmail.com>
2020-01-25 15:17:48 -0500

    about page

---
+++ frontend/pages/About.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div(class="about")
+    Explain(class="mt-5")
+</template>
+
+<script>
+import Explain from '@/components/Explain.vue'
+export default {
+  name: 'about',
+  components: {
+    Explain
+  }
+}
+</script>
+
+<style scoped></style>
