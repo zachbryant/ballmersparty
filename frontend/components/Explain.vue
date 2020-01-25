c2067d835091d2cc753cf79682078e990f099a92
zachbryant <zbryant832@gmail.com>
2020-01-25 15:50:53 -0500

    asciiart explain

--- frontend/components/Explain.vue
+++ frontend/components/Explain.vue
@@ -2,32 +2,7 @@
 	div(id="explain")
 		b-container(fluid)
 			h3(id="explain_title") /**** Eh what's all this now? ****/
-			pre(id="ascii_art") .===================================================================.
-||     __   _    __   __  __   __          __   ..        __       ||
-|| -=]|__  /_\  |__) |__ |__) /  \ |\  /| |__) |  | |__/ |__ |\ |  ||
-|| -=]|   /   \ |  \ |   |  \ \__/ | \/ | |    \__/ |  \ |__ | \|  ||
-||                            ___                                  ||
-||                          .'   '.                                ||
-||                         /       \           oOoOo               ||
-||                        |         |       ,==|||||               ||
-||                         \       /       _|| |||||               ||
-||                          '.___.'    _.-'^|| |||||               ||
-||                        __/_______.-'     '==HHHHH               ||
-||                   _.-'` /                   """""               ||
-||                .-'     /   oOoOo                                ||
-||                `-._   / ,==|||||                                ||
-||                    '-/._|| |||||                                ||
-||                     /  ^|| |||||                                ||
-||                    /    '==HHHHH                                ||
-||                   /________"""""                                ||
-||                   `\       `\                                   ||
-||                     \        `\   /                             ||
-||                      \         `\/                              ||
-||                      /                                          ||
-||                     /                                           ||
-||               jgs  /_____                                       ||
-||                                                                 ||
-'==================================================================='
+			pre(id="ascii_art") {{asciiart}}
 			p(style="text-align: justify; text-shadow: none;") &emsp;Programmers have a bit of a reputation for drinking. If (or when) you've had to read someone else's 
 				a(class="underline" href="https://imgs.xkcd.com/comics/code_quality.png" target="_blank") soul sucking documentation for work, you'll understand.
 				span The peak is named after Steve Ballmer (the former CEO of Microsoft) for his sudden outbursts and outlandish public behavior, which make him seem constantly intoxicated. It is rumored that the peak lies at approximately 0.1337% BAC, but it varies for individuals. 
@@ -39,7 +14,17 @@
 
 </template>
 
-<script></script>
+<script>
+import asciiart from '../assets/asciiart'
+export default {
+  name: 'explain',
+  data() {
+    return {
+      asciiart
+    }
+  }
+}
+</script>
 
 <style>
 #explain {
