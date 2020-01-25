d94162bf5ff9fa86212a1938a87cd021e9ce7f47
zachbryant <zbryant832@gmail.com>
2020-01-25 17:17:29 -0500

    touchups

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
-  footer(class="footer")
+  footer(class="footer mt-5")
     b-container(fluid class="container")
       p(class="note") Made with 
-        i(class="fa fa-heart" style="font-size:16px;color:white;")
+        i(class="fa fa-heart" style="font-size:16pxl;")
         span(class="note")  at Purdue.
 </template>
 
@@ -13,7 +13,7 @@
   justify-content: center;
   align-items: center;
   text-align: center;
-  position: absolute;
+  position: relative;
   bottom: 0;
   width: 100%;
   height: 60px;
