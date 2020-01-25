c3e48e1f4a73f7968972f4461ffe71c729dc3b37
zachbryant <zbryant832@gmail.com>
2020-01-25 15:12:41 -0500

    intro chart component

---
+++ frontend/components/IntroChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="chart-container"
+    style="position: relative; height:75vh; width:60vw"
+  >
+    <canvas id="chart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+import homeChartData from '../assets/home-chart-data.js'
+const canvas = document.getElementById('chart')
+export default {
+  name: 'IntroChart',
+  data() {
+    return {
+      homeChartData
+    }
+  },
+  methods: {
+    createChart(chartId, chartData) {
+      const canvas = document.getElementById(chartId)
+      const myChart = new Chart(canvas, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options
+      })
+      myChart.resize()
+    }
+  },
+  mounted() {
+    this.createChart('chart', this.homeChartData)
+  }
+}
+</script>
+
+<style scoped></style>
