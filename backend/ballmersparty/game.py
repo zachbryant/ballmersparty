31731cf6309ca22581eb3d83f89b69466ac51e4a
Noah Alderton <noahlouisalderton@gmail.com>
2020-01-25 13:03:04 -0500

    Added some basic GameManager functions

--- backend/ballmersparty/game.py
+++ backend/ballmersparty/game.py
@@ -2,32 +2,72 @@ from typing import Dict
 
 from .user import User
 
+import random
 
 class GameManager:
 
-    current_games: Dict[str, "GameSession"] = {}
+    CODE_LENGTH = 4 # Length of the generated game party codes
+
+    current_games: Dict[str, "GameSession"] = {} # Active games
+
+    # Create a game party code
+    def generate_code(self):
+        letters = string.ascii_uppercase
+        return ''.join(random.choice(letter) for i in range(CODE_LENGTH))
 
     def create_game(self, user: User, options) -> "GameSession":
-        raise NotImplementedError
+        join_code = generate_code()
 
-    def join_game(self, user: User, code):
-        if code not in self.current_games:
+        # Checks if party code already exists
+        while join_code not in self.current_games:
+            join_code = generate_code()
+        
+        session = GameSession(join_code, user)
+
+        current_games.add(join_code, session)
+
+        return join_code
+
+    def join_game(self, user: User, join_code: str):
+        if join_code not in self.current_games:
             return False
+        self.current_games[join_code].add_user(user)
+        return True
 
-        self.current_games[code].add_user(user)
+    def delete_game(self, join_code: str):
+        current_game = current_games.pop(join_code)
+        current_game.stop_game()
 
 
 class GameState:
+
     pass
 
 
 class GameSession:
     join_code: str
-    options: object
+    party_master: str
 
-    def __init__(self, options):
-        self.options = options
+    users = []
+
+    def __init__(self, join_code, party_master):
+        if type(join_code) not str  or type(party_master) not str:
+            raise TypeError
+
+        self.join_code = join_code
+        self.party_master = party_master
         self.game_state = GameState()
 
     def add_user(self, user: User):
-        raise NotImplementedError
+        users.add(user)
+
+    def remove_user(self, user: User):
+        users.remove(user)
+
+    def stop_game(self, join_code):
+        for user in users:
+            user.endGame()
+    
+
+
+        
