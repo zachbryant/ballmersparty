80fd2cb554353ec8f6143a1025e1d8edb6512382
elnardu <elnardu2@gmail.com>
2020-01-25 23:33:03 -0500

    Stuff

--- backend/ballmersparty/config.py
+++ backend/ballmersparty/config.py
@@ -1,1 +1,13 @@
+import os
+
+
 SERVE_DIST = False
+
+PROBLEMS_PATH = (
+    os.environ["PROBLEMS_PATH"]
+    if "PROBLEMS_PATH" in os.environ
+    else os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "..", "..", "problem_tester", "problems"
+    )
+)
+
