e9dea8adf330bca887d35bcfa721548288dd561f
Noah Alderton <noahlouisalderton@gmail.com>
2020-01-25 21:01:35 -0500

    Created Docker image that returns # of passed cases

--- problem_tester/problems/loops/logic.py
+++ problem_tester/problems/loops/logic.py
@@ -9,4 +9,4 @@ Problem   : https://www.hackerrank.com/challenges/python-loops/problem
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 a=int(raw_input())
 for i in range(0,a):
-    print i*i
+    print(i*i)
--- problem_tester/problems/modDivmod/logic.py
+++ problem_tester/problems/modDivmod/logic.py
@@ -10,6 +10,6 @@ Problem   : https://www.hackerrank.com/challenges/python-mod-divmod/problem
 a=int(raw_input())
 b=int(raw_input())
 d=divmod(a,b)
-print d[0]
-print d[1]
-print d
+print(d[0])
+print(d[1])
+print(d)
